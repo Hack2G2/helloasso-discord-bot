20373318bffd51ac113e03caf30515b3861ffa56
xanhacks <xanhacks@protonmail.com>
2021-09-20 09:29:44 +0200

    split project into 2 parts

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 #!/usr/bin/env python3
-import os
-
-import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} is ready !')
-
-client.run(TOKEN)
+from lib import http_server, bot
 
